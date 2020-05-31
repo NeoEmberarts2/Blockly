cf6cbc25b1460a2b73ac8591e8748d7dacad4986
NeoEmberarts2 <57146384+NeoEmberarts2@users.noreply.github.com>
2020-05-31 13:46:27 -0400

    CUTOM blocks version 1

---
+++ Block code.js
@@ -0,0 +1,6 @@
+Blockly.JavaScript['new'] = function(block) {
+  var text_code123 = block.getFieldValue('Code123');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
---
+++ Block def.js
@@ -0,0 +1,16 @@
+[{
+  "type": "new",
+  "message0": "code =  %1",
+  "args0": [
+    {
+      "type": "field_input",
+      "name": "Code123",
+      "text": "HI"
+    }
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 230,
+  "tooltip": "",
+  "helpUrl": ""
+}]
